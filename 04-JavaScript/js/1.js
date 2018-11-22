0e33aef81776e385e8e4b6fe3df29f192ca05ae5
huoyingmi <1158054985@qq.com>
2018-11-22 19:34:23 +0800

    04-javascript

---
+++ 04-JavaScript/js/1.js
@@ -0,0 +1,9 @@
+function toChange(){
+	var oBox = document.getElementById('box');
+	oBox.style.width='400px';
+	oBox.style.height='400px';
+	oBox.style.borderTopColor='red';
+	oBox.style.background='blue';
+}
+var oBox = document.getElementById('box');
+oBox.onclick=toChange;
---
+++ 04-JavaScript/js/2.js
@@ -0,0 +1,8 @@
+function toChange(){
+	var oBox = document.getElementById('box');
+	oBox.style.width='400px';
+	oBox.style.height='400px';
+	oBox.style.borderTopColor='red';
+}
+var oBox = document.getElementById('box');
+oBox.onclick=toChange;
