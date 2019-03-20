c75c201fd677227cda4c7a86b961561c0910d993
huoyingmi <1158054985@qq.com>
2019-03-20 18:32:11 +0800

    18:31//2

---
+++ 012-nodejs/file/m03.js
@@ -0,0 +1,6 @@
+const str = 'hello';
+// console.log(global);
+global.str = str;
+
+// console.log('m03...');
+// console.log(global);
---
+++ 012-nodejs/file/m06.js
@@ -0,0 +1,5 @@
+//require('./m05.js');  //require是一个函数
+
+const m05 = require('./m05.js');
+// console.log("m06::",m05); 
+//导出了两次，说明返回值就是导出的对象
