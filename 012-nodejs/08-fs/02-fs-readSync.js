ddb21c0f7eaab9727fab9be9b58ed928675198d0
huoyingmi <1158054985@qq.com>
2019-03-22 20:58:47 +0800

    /2

--- 012-nodejs/08-fs/02-fs-readSync.js
+++ 012-nodejs/08-fs/02-fs-readSync.js
@@ -1,29 +1,2 @@
 // 同步可读流
 
-const {Readable} = require('fs');
-// 继承
-class MyReader extends Readable{
-	constructor(){
-		super();
-		this.index = 0;
-	}
-	_read(){
-		this.index++;
-		if(this.index > 5){
-			this.push(null);
-		}else{
-			this.push(this.index);
-		}
-	}
-}
-
-const reader = new MyReader();
-reader.on('data',(chunk)=>{
-	console.log(chunk.toString());
-})
-
-reader.on('end',()=>{
-	console.log('end...');
-})
-
-// reader.pipe(process.stream);
